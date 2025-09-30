b71d87ffaed25ee0b1f6f2e61bee71c318ceecf9
Daniela Sanvicente <d.sanvicente.e@gmail.com>
2025-09-30 12:06:40 -0600

    fixes userSecret things

--- Academy.Users.Infrastructure/Context/ApplicationDbContextFactory.cs
+++ Academy.Users.Infrastructure/Context/ApplicationDbContextFactory.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.UserSecrets;
 
 namespace Academy.Users.Infrastructure.Context;
 
@@ -8,10 +9,14 @@ public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<Applicati
 {
     public ApplicationDbContext CreateDbContext(string[] args)
     {
+        const string userSecretsId = "12f1aa0f-5f6a-4389-9a6f-9c719d1ce4ad";
+
         var configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json")
             .AddJsonFile("appsettings.Development.json", optional: true)
+            .AddUserSecrets(userSecretsId, optional: true)
+            .AddEnvironmentVariables()
             .Build();
 
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
